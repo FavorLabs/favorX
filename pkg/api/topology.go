146964eddc0463aad14b79254420eaeece6dfd96
Barnett <Barnett@favorlabs.io>
2022-06-21 14:27:35 +0800

    fix: restore the p2p connection

--- pkg/api/topology.go
+++ pkg/api/topology.go
@@ -3,11 +3,9 @@ package api
 import (
 	"net"
 	"net/http"
-	"time"
 
 	"github.com/gauss-project/aurorafs/pkg/boson"
 	"github.com/gauss-project/aurorafs/pkg/jsonhttp"
-	"github.com/gauss-project/aurorafs/pkg/topology"
 )
 
 func (s *server) apiPort(w http.ResponseWriter, r *http.Request) {
@@ -34,28 +32,13 @@ func (s *server) apiPort(w http.ResponseWriter, r *http.Request) {
 	jsonhttp.OK(w, resp)
 }
 
-func (s *server) saveConnectedPeers() {
-	go func() {
-		t := time.NewTicker(time.Second * 10)
-		defer t.Stop()
-		for {
-			select {
-			case <-t.C:
-				var now []boson.Address
-				_ = s.kad.EachPeer(func(address boson.Address, u uint8) (stop, jumpToNext bool, err error) {
-					now = append(now, address)
-					return false, false, nil
-				}, topology.Filter{Reachable: false})
-				s.snapshotPeers = now
-			}
-		}
-	}()
-}
-
 func (s *server) restore(w http.ResponseWriter, r *http.Request) {
-	list := s.snapshotPeers
-	for _, v := range list {
-		_ = s.route.Connect(r.Context(), v)
-	}
+	_ = s.kad.EachKnownPeer(func(address boson.Address, u uint8) (stop, jumpToNext bool, err error) {
+		ss := s.kad.SnapshotAddr(address)
+		if ss.ConnectionTotalDuration > 0 {
+			_ = s.route.Connect(r.Context(), address)
+		}
+		return false, false, nil
+	})
 	jsonhttp.OK(w, nil)
 }
