8725a7349bb8358cbc32074729f56152446349f1
CooperFL <cooper@favorlabs.io>
2022-07-01 10:38:23 +0800

    chore: define separately wiredtiger options in different platforms

--- pkg/shed/wiredtiger/driver.go
+++ pkg/shed/wiredtiger/driver.go
@@ -45,23 +45,6 @@ import (
 // In general the driver is used via the shed/driver package.
 type Driver struct{}
 
-var (
-	defaultCreate        = true
-	defaultCacheSize     = DiskSize{Size: 2, Type: GB}
-	defaultCacheOverhead = 8
-	defaultCheckpoint    = Checkpoint{LogSize: DiskSize{Size: 2, Type: GB}, Wait: 60}
-	defaultConfigBase    = false
-	defaultDebugMode     = DebugMode{CheckpointRetention: 0, CursorCopy: false, Eviction: false, TableLogging: false}
-	defaultEviction      = Eviction{ThreadsMin: 4, ThreadsMax: 4}
-	defaultFileManager   = FileManger{CloseIdleTime: 600, CloseScanInterval: 10, CloseHandleMinimum: 2000}
-	defaultLog           = Log{Enabled: true, Archive: true, Path: "journal", Compressor: SnappyCompressor}
-	defaultSessionMax    = 33000
-	defaultStatistics    = []StatisticsPolicy{StatisticsFast}
-	defaultStatisticsLog = StatisticsLog{Wait: 0}
-	defaultExtensions    = "[/usr/local/lib/libwiredtiger_snappy.so]"
-	defaultVerbose       = "[recovery_progress,checkpoint_progress,compact_progress]"
-)
-
 func checkDirectory(path string) error {
 	f, err := os.Open(path)
 	if err != nil {
---
+++ pkg/shed/wiredtiger/options_darwin.go
@@ -0,0 +1,18 @@
+package wiredtiger
+
+var (
+	defaultCreate        = true
+	defaultCacheSize     = DiskSize{Size: 1, Type: GB}
+	defaultCacheOverhead = 12
+	defaultCheckpoint    = Checkpoint{LogSize: DiskSize{Size: 2, Type: GB}, Wait: 60}
+	defaultConfigBase    = false
+	defaultDebugMode     = DebugMode{CheckpointRetention: 0, CursorCopy: false, Eviction: false, TableLogging: false}
+	defaultEviction      = Eviction{ThreadsMin: 4, ThreadsMax: 4}
+	defaultFileManager   = FileManger{CloseIdleTime: 600, CloseScanInterval: 10, CloseHandleMinimum: 2000}
+	defaultLog           = Log{Enabled: true, Archive: true, Path: "journal", Compressor: SnappyCompressor}
+	defaultSessionMax    = 20
+	defaultStatistics    = []StatisticsPolicy{StatisticsFast}
+	defaultStatisticsLog = StatisticsLog{Wait: 0}
+	defaultExtensions    = "[/usr/local/lib/libwiredtiger_snappy.so]"
+	defaultVerbose       = "[]"
+)
