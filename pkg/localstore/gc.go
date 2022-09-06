c63bd176fc0921da795948b1403ce698c5c8b224
logan <logan@favorlabs.io>
2022-09-06 17:48:50 +0800

    perf: light nodes no longer trigger memory GC.

--- pkg/localstore/gc.go
+++ pkg/localstore/gc.go
@@ -296,10 +296,12 @@ func (db *DB) incGCSizeInBatch(batch driver.Batching, change int64) (err error) 
 	if newSize >= db.capacity {
 		db.triggerGarbageCollection()
 	}
-	//m := getMemory()
-	//if gcMemory > m {
-	//	db.triggerGarbageMemoryCollection()
-	//}
+	if db.fullNode {
+		m := getMemory()
+		if gcMemory > m {
+			db.triggerGarbageMemoryCollection()
+		}
+	}
 	return nil
 }
 
